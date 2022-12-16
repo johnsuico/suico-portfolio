cb82befbdd2d1d322842ef0ed1034213dd919481
John Suico <johncdsuico@gmail.com>
2022-12-16 13:44:19 -0800

    Finish hero section

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 // Import CSS
 import './Landing.css';
+
+import { BsGithub } from 'react-icons/bs';
 
 // Import Navbar
 import { Navbar } from '../';
@@ -10,6 +13,19 @@ const Landing = () => {
   return (
     <div className="landing-container">
       <Navbar />
+      <div className="landing-content-container">
+        <div className="hero-content">
+          <h2 className="hero-title">Hi, I'm <span>John Suico.</span></h2>
+          <p className="hero-caption">Computer Engineer and Full Stack Developer.</p>
+          <div className="hero-buttons">
+            <Link to="/" className="hero-link">Contact Me</Link>
+            <div className="github-btn">
+              <BsGithub className="github-icon" />
+              <a href="https://github.com/johnsuico" target="_blank"  rel="noreferrer">GitHub</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
