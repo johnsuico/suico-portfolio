a4f2df287d33717b64d8aa6075e8e8f55cb3b83f
John Suico <johncdsuico@gmail.com>
2022-12-16 13:57:52 -0800

    Create skills

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className="skills-container">
+      <h2 className="skills-title">Skills</h2>
+      <div className="skills-icon-container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Skills
