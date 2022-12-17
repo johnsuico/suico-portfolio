41f6caa93e0bd5bfbd6cf99eaa2d8da88149ffa3
John Suico <johncdsuico@gmail.com>
2022-12-17 10:13:42 -0800

    Finish contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,6 +3,8 @@ import emailjs from '@emailjs/browser';
 
 import './Contact.css';
 
+import {AiTwotoneMail, AiFillLinkedin} from 'react-icons/ai';
+
 const Contact = () => {
 
   const [sent, setSent] = useState(false);
@@ -39,32 +41,42 @@ const Contact = () => {
 
   return (
     <div className="contact-container">
-      <h2 className="project-section-title">Contact</h2>
+      <h2 className="project-section-title" id="contact">Contact</h2>
 
-      {sent ? 
-        <div className="email-notif-container">
-          <p className="email-caption email-sent">Message sent successfully</p>
+      <div className="contact-info-container">
+        <div className="contact-links">
+          <div className="contact-link-group">
+            <AiTwotoneMail className="contact-email-icon"/>
+            <p className="contact-text">johncdsuico@gmail.com</p>
+          </div>
         </div>
-      :
-        null
-      }
-
-      {err ? 
         <div className="email-notif-container">
-          <p className="email-caption email-err">Something went wrong.</p>
-        </div>
-      :
-        null
-      }
+          { sent ?
+            <p className="email-sent">Email sent successfully.</p>
+          :
+            null
+          }
 
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+          { err ? 
+            <p className="email-err">Something went wrong. Try emailing manually.</p>
+          :
+            null
+          }
+        </div>
+      </div>
+
+
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
+        <label className="contact-form-label">Name</label>
+        <input type="text" name="user_name" className="contact-form-input" placeholder="Your name"/>
+
+        <label className="contact-form-label">Email</label>
+        <input type="email" name="user_email" className="contact-form-input" placeholder="Your email"/>
+
+        <label className="contact-form-label">Message</label>
+        <textarea name="message" className="contact-form-text-area" />
+
+        <input type="submit" value="Send" className="contact-form-btn" />
       </form>
     </div>
   )
